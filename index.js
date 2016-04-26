ac23da71bd938e56765f322b5dd15ed23cee946f
Cal Rueb <calvin.c.rueb@gmail.com>
2016-04-26 14:39:41 -0500

    message broadcasted when new user connects

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+ socket.broadcast.emit('new user');
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg)
+    io.emit('chat message', msg);
   });
 });
 
